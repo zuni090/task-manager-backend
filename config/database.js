c1791d216e28bffd7ccb7b17188cac76524ebb70
Muhammad Zunique <89668040+zuni090@users.noreply.github.com>
2023-12-25 00:36:45 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -6,7 +6,6 @@ const connectDatabase = async() =>{
         const conn = await mongoose.connect(process.env.MONGO_URI ,  {
             useNewUrlParser: true, // Note: useNewUrlParser is no longer needed, but you can keep it for now
           useUnifiedTopology: true, // Note: useUnifiedTopology is no longer needed, but you can keep it for now
-          useFindAndModify: false,
             w: "majority"
         });
 
